ca4062d3d1034794dfb396024ae843e845c78aa6
unknown <mihaela.nicoleta.jecu@gmail.com>
2022-04-27 16:40:10 +0300

    Revert "am mancat painea"
    This reverts commit 573c96e1bfbba0eff6df0de00fbf4325cd97433b.

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ const App = () => {
   console.log("2");
   console.log("3");
   console.log("4");
-  console.log("5");
-  console.log("6");
   return (
     <div className="categories-container">
       <div className="category-container">
