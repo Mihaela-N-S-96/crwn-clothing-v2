fbc1dc6ff4dcc9d8100e74f7a33c67bf99c7ddef
unknown <mihaela.nicoleta.jecu@gmail.com>
2022-04-27 16:35:10 +0300

    am facut paine
    commit1

--- src/App.js
+++ src/App.js
@@ -1,37 +1,41 @@
 const App = () => {
+  console.log("1");
+  console.log("2");
+  console.log("3");
+  console.log("4");
   return (
-    <div className='categories-container'>
-      <div className='category-container'>
+    <div className="categories-container">
+      <div className="category-container">
         {/* <img /> */}
-        <div className='category-body-container'>
+        <div className="category-body-container">
           <h2>Hats</h2>
           <p>Shop Now</p>
         </div>
       </div>
-      <div className='category-container'>
+      <div className="category-container">
         {/* <img /> */}
-        <div className='category-body-container'>
+        <div className="category-body-container">
           <h2>Jackets</h2>
           <p>Shop Now</p>
         </div>
       </div>
-      <div className='category-container'>
+      <div className="category-container">
         {/* <img /> */}
-        <div className='category-body-container'>
+        <div className="category-body-container">
           <h2>Sneakers</h2>
           <p>Shop Now</p>
         </div>
       </div>
-      <div className='category-container'>
+      <div className="category-container">
         {/* <img /> */}
-        <div className='category-body-container'>
+        <div className="category-body-container">
           <h2>Womens</h2>
           <p>Shop Now</p>
         </div>
       </div>
-      <div className='category-container'>
+      <div className="category-container">
         {/* <img /> */}
-        <div className='category-body-container'>
+        <div className="category-body-container">
           <h2>Mens</h2>
           <p>Shop Now</p>
         </div>
